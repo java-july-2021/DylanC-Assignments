d74dc49a76aeb21cb08dcaec03bb07249fecefc5
Dylan Collare <dcollare@gmail.com>
2021-07-07 15:11:43 -0400

    Pythagorean Theorem Assignment

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythTheorem.java
@@ -0,0 +1,6 @@
+import java.lang.Math;
+public class PythTheorem {
+    public double calculateHypotenuse(int legA, int legB){
+        return Math.sqrt(legA * legA + legB * legB);
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythTheoremTest.java
@@ -0,0 +1,6 @@
+public class PythTheoremTest{
+    public static void main(String[]args){
+        PythTheorem result = new PythTheorem();
+        System.out.println(result.calculateHypotenuse(5, 4)); 
+    }
+}
