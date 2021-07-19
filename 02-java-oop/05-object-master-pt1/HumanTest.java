b7781421fc8e0675c7228fc7788df8969770b8b5
Dylan Collare <dcollare@gmail.com>
2021-07-18 20:53:22 -0400

    Object Mater Pt1 Assignment

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,13 @@
+public class HumanTest{
+    public static void main(String[]args){
+        Human character1 = new Human(100, 3);
+        Human character2 = new Human(100, 3);
+
+        character1.attack(character2);
+        character2.health();
+        character1.attack(character2);
+        character1.attack(character2);
+        character1.attack(character2);
+        character2.health();
+    }
+} 
